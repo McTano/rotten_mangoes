c9920a332958bf2b77169db57f4819c1a00f353b
tristan <tsmackinlay@gmail.com>
2016-06-21 12:48:09 -0700

    step 5 done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,18 @@
 class Movie < ActiveRecord::Base
+
+  validates_presence_of :title, :director, :description, :poster_image_url, :release_date
+
+  validates :runtime_in_minutes,
+    numericality: { only_integer: true }
+
+  validate :release_date_is_in_the_past
+
+  protected
+
+  def release_date_is_in_the_past
+    if release_date.present?
+      errors.add(:release_date, "should be in the past") if release_date > Date.today
+    end
+  end
+
 end
