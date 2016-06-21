0dffafc53d4464a228a22822acbc4ae67f8847b1
tristan <tsmackinlay@gmail.com>
2016-06-21 11:35:40 -0700

    added routes

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,52 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update_attributes(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  protected
+
+  def movie_params
+    params.require(:movie).permit(
+      :title, :release_date, :director, :runtime_in_minutes, :poster_image_url,
+      )
+  end
+
+end
