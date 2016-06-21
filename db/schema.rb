1953cc0d49ca661754ab653eee0a25d44fde5493
tristan <tsmackinlay@gmail.com>
2016-06-21 13:19:30 -0700

    step 7

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621180346) do
+ActiveRecord::Schema.define(version: 20160621201304) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20160621180346) do
     t.datetime "release_date"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
